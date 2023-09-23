61882b205ac0bc624d4185c075e14037b03baad2
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-09-23 18:20:06 +0700

    add sapce

--- app/components/Stacks.tsx
+++ app/components/Stacks.tsx
@@ -11,7 +11,7 @@ export default function Stacks (props: {title: string, list: {imgUrl: string, bg
             <h1 className="text-white font-semibold text-lg">{title}</h1>        
         </div>
 
-        <div className="grid grid-cols-7 w-full gap-3">
+        <div className="grid grid-cols-7  w-full gap-3">
             {props.list.map((l, i) => <div key={"stack " + i} className={`flex gap-3 p-1 w-10 h-10 justify-center items-center rounded-full ${l.bgColor}`}>
                 <Image className="w-full" src={l.imgUrl} height={25} width={25} alt={"icon of " + props.title}/>
             </div>)}
