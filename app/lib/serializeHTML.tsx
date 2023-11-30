439be2b44a6a16bd197fdc1bd218160b62a7eaa3
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-11-30 08:06:55 +0000

    add headings

--- app/lib/serializeHTML.tsx
+++ app/lib/serializeHTML.tsx
@@ -38,7 +38,26 @@ export const serializeHTML = (children: any[]) =>
     switch (node.type) {
       case "h1":
         return <h1 key={i}>{serializeHTML(node.children)}</h1>;
-      // Iterate through all headings here...
+      case "h2":
+        return <h2 key={i}>{serializeHTML(node.children)}</h2>;
+      case "h3":
+        return (
+          <h3 className="text-xl font-semibold" key={i}>
+            {serializeHTML(node.children)}
+          </h3>
+        );
+      case "h4":
+        return (
+          <h4 className="text-xl font-semibold" key={i}>
+            {serializeHTML(node.children)}
+          </h4>
+        );
+      case "h5":
+        return (
+          <h5 className="font-semibold" key={i}>
+            {serializeHTML(node.children)}
+          </h5>
+        );
       case "h6":
         return <h6 key={i}>{serializeHTML(node.children)}</h6>;
       case "blockquote":
