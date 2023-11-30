081f2bbe6cab672162e3de9869ed0ce1d5a8f9fc
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-11-30 03:14:16 +0000

    fix error typing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Image from "next/image";
 import Navbar from "./layout/Navbar";
-import { Icon } from "next/dist/lib/metadata/types/metadata-types";
 
 const inter = Inter({ subsets: ["latin"] });
 
